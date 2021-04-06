eaced7912879c834adc0c40694606032a8b4cea9
Carlo Stanzione <60319265+VinStan1@users.noreply.github.com>
2021-04-06 19:13:55 +0200

    Add files via upload

---
+++ _nuxt/13b2320.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{251:function(e,n,t){"use strict";t.r(n);var l={middleware:"admin"},r=t(35),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("ADMIN SECTION")])}),[],!1,null,null,null);n.default=component.exports}}]);
